516657b4c490cbef38c81a29dfc3754b2bba2a46
season <sionwer4@naver.com>
2023-10-10 22:37:45 +0900

    feat: Add gateway route

--- first-service/src/main/java/com/example/firstservice/controller/HealthCheckController.java
+++ first-service/src/main/java/com/example/firstservice/controller/HealthCheckController.java
@@ -1,15 +1,24 @@
 package com.example.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class HealthCheckController {
 
     @GetMapping("/welcome")
     public String heathCheck(){
         return "Welcome to first server";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info("header={}", header);
+        return "First service message";
     }
 }
