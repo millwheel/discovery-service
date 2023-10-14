7c57ca134b224ff3cac5f991157e28732e61fa38
sijun park <sionwer4@naver.com>
2023-10-14 22:38:39 +0900

    feat(user): Add user create method in user controller

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.userservice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
-@AllArgsConstructor
 public class UserController {
 
     private final Greeting greeting;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(Greeting greeting, UserService userService) {
+        this.greeting = greeting;
+        this.userService = userService;
+    }
 
     @GetMapping("health-check")
     public String status(){
@@ -21,6 +30,15 @@ public class UserController {
     @GetMapping("/welcome")
     public String welcome(){
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+        return "user created";
     }
 
 }
