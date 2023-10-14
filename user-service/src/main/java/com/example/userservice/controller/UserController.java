35dedc1e3c431553293cea6cbf4e057d756cbe4f
sijun park <sionwer4@naver.com>
2023-10-14 15:14:48 +0900

    feat: Add greeting message to controller using environment

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class UserController {
 
+    private final Environment env;
+
+    @Autowired
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("health-check")
     public String status(){
         return "The user service is activated successfully.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return env.getProperty("greeting.message");
     }
 }
