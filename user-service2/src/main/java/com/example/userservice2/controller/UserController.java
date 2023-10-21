d078d4c5282eda23e6b3dc766ebdc065bc342986
sijun park <sionwer4@naver.com>
2023-10-21 13:13:22 +0900

    feat: Create user service using spring boot 2

---
+++ user-service2/src/main/java/com/example/userservice2/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.userservice2.controller;
+
+import com.example.userservice2.dto.UserDto;
+import com.example.userservice2.service.UserService;
+import com.example.userservice2.vo.Greeting;
+import com.example.userservice2.vo.RequestUser;
+import com.example.userservice2.vo.ResponseUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final Greeting greeting;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(Greeting greeting, UserService userService) {
+        this.greeting = greeting;
+        this.userService = userService;
+    }
+
+    @GetMapping("health-check")
+    public String status(){
+        return "The user service is activated successfully.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseUser createUser(@RequestBody RequestUser user){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+        return mapper.map(userDto, ResponseUser.class);
+    }
+
+}
