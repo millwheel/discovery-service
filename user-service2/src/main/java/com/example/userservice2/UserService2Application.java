56a98d7cb416edeed1491712bfd82d3a613a9da9
sijun park <sionwer4@naver.com>
2023-10-22 18:24:49 +0900

    feat(user): Add encrypted password encoder

--- user-service2/src/main/java/com/example/userservice2/UserService2Application.java
+++ user-service2/src/main/java/com/example/userservice2/UserService2Application.java
@@ -3,6 +3,8 @@ package com.example.userservice2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +12,11 @@ public class UserService2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(UserService2Application.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
--- user-service2/src/main/java/com/example/userservice2/service/UserServiceImpl.java
+++ user-service2/src/main/java/com/example/userservice2/service/UserServiceImpl.java
@@ -3,18 +3,26 @@ package com.example.userservice2.service;
 import com.example.userservice2.dto.UserDto;
 import com.example.userservice2.entity.UserEntity;
 import com.example.userservice2.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -23,7 +31,7 @@ public class UserServiceImpl implements UserService{
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("encrypted_password");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
         userRepository.save(userEntity);
 
         return mapper.map(userEntity, UserDto.class);
