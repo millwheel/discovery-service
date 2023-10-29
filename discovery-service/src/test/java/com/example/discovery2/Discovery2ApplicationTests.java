eb42eb318a1515e1b4037c02b8d113d68b07ad1c
season <sionwer4@naver.com>
2023-10-29 15:54:20 +0900

    refactor: Change directory folder name

---
+++ discovery-service/src/test/java/com/example/discovery2/Discovery2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discovery2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Discovery2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
