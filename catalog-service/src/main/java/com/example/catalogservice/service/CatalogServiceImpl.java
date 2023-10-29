db4c8c914e51b42152c83dc15994aa604e865144
season <sionwer4@naver.com>
2023-10-29 18:17:54 +0900

    feat(catalog): Create catalog service

---
+++ catalog-service/src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.catalogservice.service;
+
+import com.example.catalogservice.entity.CatalogEntity;
+import com.example.catalogservice.repository.CatalogRepository;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Data
+@Slf4j
+@Service
+public class CatalogServiceImpl implements CatalogService{
+
+    CatalogRepository catalogRepository;
+
+    public CatalogServiceImpl(CatalogRepository catalogRepository) {
+        this.catalogRepository = catalogRepository;
+    }
+
+    @Override
+    public Iterable<CatalogEntity> getAllCatalogs() {
+        return catalogRepository.findAll();
+    }
+}
