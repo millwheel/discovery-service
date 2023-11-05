28e1022389be033490902b8bdd5c3e509862aeae
season <sionwer4@naver.com>
2023-11-05 17:53:46 +0900

    feat: Enroll BCryptPasswordEncoder as bean

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -14,4 +14,9 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
 }
