bb93c5e39a1f600a7010dd929433508b45ea0ddf
season <sionwer4@naver.com>
2023-11-05 17:54:59 +0900

    feat: Implement get user by email method in user service

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -2,10 +2,10 @@ package com.example.userservice.service;
 
 
 import com.example.userservice.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
-
     UserDto getUserByUserId(String userId);
     Iterable<UserDto> getUserByAll();
 }
